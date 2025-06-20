1fad8931c9564795dd1debcbf573e6a8e3171fb5
Raquel Lacorte <costa.lacorte@gmail.com>
2025-06-20 17:48:48 +0100

    Solved lab

--- src/add.js
+++ src/add.js
@@ -5,3 +5,16 @@ function add (numOne, numTwo) {
 
     return numOne + numTwo;
 }
+it("should return undefined if one or both arguments are not numbers", function () {
+  expect(add("5", 2)).toBeUndefined();
+  expect(add(5, "2")).toBeUndefined();
+  expect(add("5", "2")).toBeUndefined();
+  expect(add(5)).toBeUndefined();
+  expect(add()).toBeUndefined();
+});
+function add(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return undefined;
+  }
+  return a + b;
+}
--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,6 @@
-function calculateArea() {
-
+function calculateArea(width, height) {
+  if (typeof width !== "number" || typeof height !== "number") {
+    return undefined;
+  }
+  return width * height;
 }
